3933c8e9c03152089cc9404f78639a7a7df02e23
adekunle22k <106889375+adekunle22k@users.noreply.github.com>
2023-09-03 03:20:42 +0100

    omo

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -14,6 +14,7 @@ const CanvasModel = () => {
       className="w-full max-w-full h-full transition-all ease-in"
     >
       <ambientLight intensity={0.5} />
+      {/* this is what is causing the bug */}
       {/* <Environment preset="city" /> */}
 
       <CameraRig>
